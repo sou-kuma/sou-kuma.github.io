2f1616c8a47606b578d77c2f8e13db71856e3b3b
sou-kuma <64810950+sou-kuma@users.noreply.github.com>
2020-05-05 06:27:26 +0900

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,41 @@
+'use strict';
+
+{
+  const likeComponent = Vue.extend({
+    props: {
+      message: {
+        type: String,
+        default: 'Like'
+      }
+    },
+    data: function() {
+      return {
+        count: 0
+      }
+    },
+    template: '<button @click="countUp">{{ message}} {{ count }}</button>',
+    methods: {
+      countUp: function() {
+        this.count++;
+        this.$emit('increment');
+      }
+    }
+  })
+
+
+  const app = new Vue({
+    el: '#app',
+    components: {
+      'like-component': likeComponent
+    },
+    data: {
+      total: 0
+    },
+    methods: {
+      incrementTotal: function() {
+        this.total++;
+      }
+    }
+  });
+
+}
